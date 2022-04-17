1851bce750c488cf1f2f9f0ca76bc808660249aa
luksdev <lucasarinisio@hotmail.com>
2022-04-17 09:59:31 -0300

    fix colormode

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,8 +1,16 @@
 import { extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
 
 const config = {
   initialColorMode: "dark",
   useSystemColorMode: false,
+  styles: {
+    global: (props: any) => ({
+      body: {
+        bg: mode("white", "black")(props),
+      },
+    }),
+  },
 };
 
 const theme = extendTheme({ config });

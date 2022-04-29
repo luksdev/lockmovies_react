073432605114e39b6de6f4c83ab57debd9654365
Lucas Eduardo <69233035+luksdev@users.noreply.github.com>
2022-04-29 19:32:48 -0300

    change method to signin

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export async function signInResquest(data: any) {
-  await axios.get(
+  await axios.post(
     "https://9947ccq2oj.execute-api.us-east-1.amazonaws.com/login_users",
     {
       withCredentials: false,

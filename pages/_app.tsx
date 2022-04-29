8d4e4a66df334d019242472a98a611da93dff7b0
Lucas Eduardo <69233035+luksdev@users.noreply.github.com>
2022-04-29 19:36:41 -0300

    type change in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../src/lib/theme";
 import { AuthProvider } from "../src/contexts/AuthContext";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: any) {
   return (
     <AuthProvider>
       <ChakraProvider theme={theme}>

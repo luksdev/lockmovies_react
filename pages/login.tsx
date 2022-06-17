7a055abf510cbb4507ca910655dd331f510daf50
luksdev <lucasarinisio@hotmail.com>
2022-06-17 19:11:57 -0300

    finish 2

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,16 @@
+import { Router, useRouter } from 'next/router';
+import { useContext } from 'react';
 import { Login } from '../src/components/Login';
+import { AuthContext } from '../src/contexts/AuthContext';
 
 export default function LoginScreen() {
+  const {user} = useContext<any>(AuthContext)
+
+  const router = useRouter();
+
+  if (user) {
+    router.push('/home');
+  }
+
   return <Login />;
 }
